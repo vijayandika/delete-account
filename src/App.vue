8cfdb7404f6755317d63cb153b55c7beab580549
vijayandika <vijayandika458@gmail.com>
2023-06-12 15:02:44 +0700

    aaa

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Delete from "./components/Delete.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Delete msg="You did it!" />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
---
+++ src/components/Delete.vue
@@ -0,0 +1,88 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="container">
+    <h1>Delete Account</h1>
+    <p>Please select a reason for deleting your account:</p>
+    <form id="delete-form">
+      <div class="form-group">
+        <label for="reason">Reason:</label>
+        <select id="reason" required>
+          <option value="">Select a reason</option>
+          <option value="no-longer-needed">No longer needed</option>
+          <option value="privacy-concerns">Privacy concerns</option>
+          <option value="unsatisfied-service">
+            Unsatisfied with the service
+          </option>
+          <option value="moving-platforms">Moving to another platform</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <button type="submit">Delete Account</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 20px;
+}
+
+h1 {
+  color: #333;
+  font-size: 24px;
+}
+
+.container {
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+select {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #555;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
